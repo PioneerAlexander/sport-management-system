fcf42b768c57d364aa4d3404e94a0d0409c2993b
Maksim <erolaz@outlook.com>
2021-11-19 12:42:22 +0300

    add kotlin-csv

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -1,5 +1,14 @@
 package ru.emkn.kotlin.sms
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+
 
 fun main(args: Array<String>) {
-    TODO()
+    //check how kotlin-csv works https://github.com/doyaaaaaken/kotlin-csv
+    csvReader().open("sample-data/applications/application1.csv") {
+        val org = Organisation(readNext()!![0])
+        readAllWithHeaderAsSequence().forEach { row: Map<String, String>->
+            org.addMember(Participant(row["Г.р"], row["Фамилия"],row["Имя"],row["Группа"],row["Разр."]))
+        }
+        println(org)
+    }
 }
