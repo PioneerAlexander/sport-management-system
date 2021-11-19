bc384a87725cb02ff523d12225419cab6de99a4a
AndreyKobetskiy <ronorothog2003@gmail.com>
2021-11-19 16:48:28 +0300

    изменена инициализация спортГрупп

--- src/main/kotlin/ru/emkn/kotlin/sms/Competition.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Competition.kt
@@ -3,20 +3,14 @@ package ru.emkn.kotlin.sms
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import java.io.File
 
-class Competition {
+class Competition (val name: String, val date: String, val orgs: List<Organisation>){
+    val participants: List<Participant>
+        get() = this.orgs.flatMap { it.members }
 
-    val sportClasses: List<SportClass>
+    val size: Int
+        get() = this.participants.size
 
-    init {
-        require(File("sample-data/classes.csv").isFile) {"No courses file"}
-        val generator = mutableListOf<SportClass>()
-        csvReader().open("sample-data/classes.csv"){
-            readNext()
-            readAllAsSequence().forEach {
-                require(it.size == 2) {"Wrong classes file"}
-                generator.add(SportClass(it[0], Distance(it[1])))
-            }
-        }
-        sportClasses = generator
-    }
+
+    val sportClasses = getSportClasses()
+
 }
