41139943596a6da9ac33e0069b40d37631f527f2
Maksim <erolaz@outlook.com>
2021-11-19 15:51:54 +0300

    competitionToStartLists надо доделать нормальную итерацию, номера участников одной длины, время (возможно kotlinx-datetime)

--- src/main/kotlin/ru/emkn/kotlin/sms/Competition.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Competition.kt
@@ -1,4 +1,9 @@
 package ru.emkn.kotlin.sms
 
-class Competition {
+class Competition (val name: String, val date: String, val orgs: List<Organisation>){
+    val participants: List<Participant>
+        get() = this.orgs.flatMap { it.members }
+
+    val size: Int
+        get() = this.participants.size
 }
--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -1,17 +1,46 @@
 package ru.emkn.kotlin.sms
-import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
-import kotlin.reflect.typeOf
 
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
+
+fun applicationToOrg(fileName: String): Organisation {
+    //check how kotlin-csv works https://github.com/doyaaaaaken/kotlin-csv
+    val org = Organisation()
+    csvReader().open(fileName) {
+        org.name = readNext()!![0]
+        readAllWithHeaderAsSequence().forEach { row: Map<String, String> ->
+            org.addMember(Participant(row["Группа"], row["Фамилия"], row["Имя"], row["Г.р."], row["Разр."]))
+        }
+    }
+    return org
+}
+
+fun competitionToStartLists(comp: Competition) {
+    val mappedParticipants = comp.participants.groupBy { it.ageGroup }
+    for ((index, category) in mappedParticipants.keys.withIndex()) {
+        csvWriter().open("testData/testStartProtocol$index.csv") {
+            writeRow(
+                listOf(category, "", "", "", "", "")
+            )
+            for ((number, participant) in mappedParticipants[category]!!.withIndex()) {
+                writeRow(
+                    listOf(
+                        "$index$number", participant.surname, participant.name,
+                        participant.birthYear, participant.sportsCategory, "time"
+                    )
+                )
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
-    //check how kotlin-csv works https://github.com/doyaaaaaken/kotlin-csv
-    csvReader().open("sample-data/applications/application1.csv") {
-        val org = Organisation(readNext()!![0])
-        readNext()
-        readAllAsSequence().forEach { row: List<String> ->
-            println( row[3])
-            org.addMember(Participant(row[0], row[1], row[2], row[3], row[4]))
-        }
-        println(org)
+    val a = mutableListOf<Organisation>()
+    for (i in 1..15) {
+        a.add(applicationToOrg("sample-data/applications/application$i.csv"))
     }
+    val comp = Competition("Test", "19.11.2021", a.toList())
+    competitionToStartLists(comp)
+
+
 }
