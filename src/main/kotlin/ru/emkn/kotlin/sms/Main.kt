7e62b1482133fe06f45c5f0bb1c1794f0a3b8046
AndreyKobetskiy <ronorothog2003@gmail.com>
2021-11-29 02:37:19 +0300

    docs

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -78,25 +78,23 @@ fun main(args: Array<String>) {
     } else {
         when (args[0]) {
             "start" -> {
-                checkArgsSize(args, 3)
+                checkArgsSize(args, 4)
                 logger.info { "переходим к созданию соревнования, получая из файла event его название и дату" }
-                val competition = makeCompetition(args[1]) //path to file event.csv
+                val competition = makeCompetition(args[1], args[3]) //path to file event.csv
 
                 competition.addOrganisationsToCompetition(args[2]) //way to directory with applications
-                competition.createStartProtocols()
-                competition.save() //saves start log in the path with pathName 'comp'
+                competition.createStartProtocols(args[3])
+                competition.save(args[3]) //saves start log in the path with pathName 'comp'
             }
             "finish" -> {
-                checkArgsSize(args, 5)
-                val competition = recreateSavedCompetition()
-
-
+                checkArgsSize(args, 6)
+                val competition = recreateSavedCompetition(args[5])
                 competition.inputTag = args[1]
                 competition.classesPath = args[2] //path to file with classes
                 competition.coursesPath = args[3] //path to file with courses
                 competition.splitsPath = args[4] //path to foldr with splits
-                generateResults(competition)
-                generateTeamResults(competition)
+                generateResults(competition, args[5])
+                generateTeamResults(competition, args[5])
 
             }
             else -> {
