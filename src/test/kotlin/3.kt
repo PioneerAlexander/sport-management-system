a8f3069acf89b1279c877d7e0de5cae764fff122
Maksim <erolaz@outlook.com>
2021-11-29 00:35:36 +0300

    test generateTeamResults

--- src/test/kotlin/3.kt
+++ src/test/kotlin/3.kt
@@ -5,7 +5,7 @@ import kotlin.test.*
 
 internal class finalResult{
     @Test
-    fun test(){
+    fun testGenerateResults(){
         val competition = recreateSavedCompetition("src/test/resources/finResTestData")
         competition.inputTag = "ByParticipantNum"
         competition.classesPath = "src/test/resources/finResTestData/classes.csv" //way to path with classes
@@ -13,6 +13,19 @@ internal class finalResult{
         competition.splitsPath =  "src/test/resources/finResTestData/splits"//path to folder with splits
         generateResults(competition, "src/test/resources/finResTestData/rezF")
         val a = File("src/test/resources/finResTestData/rezF/results.csv").readText()
+        val b = File("src/test/resources/finResTestData/results.csv").readText()
+        assertEquals(b,a)
+    }
+    @Test
+    fun testGenerateTeamResults(){
+        val competition = recreateSavedCompetition("src/test/resources/finResTestData")
+        competition.inputTag = "ByParticipantNum"
+        competition.classesPath = "src/test/resources/finResTestData/classes.csv" //way to path with classes
+        competition.coursesPath = "src/test/resources/finResTestData/courses.csv" //way to path with courses
+        competition.splitsPath =  "src/test/resources/finResTestData/splits"//path to folder with splits
+        generateResults(competition,"src/test/resources/finResTestData/rezF")
+        generateTeamResults(competition, "src/test/resources/finResTestData/rezF")
+        val a = File("src/test/resources/finResTestData/rezF/teamResults.csv").readText()
         val b = File("src/test/resources/finResTestData/results.csv").readText()
         assertEquals(b,a)
     }
