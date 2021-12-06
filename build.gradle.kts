f93b6a518d9ae6af31ee0417eae11f5def73ff06
Maksim <erolaz@outlook.com>
2021-12-06 22:20:10 +0300

    Починил тесты, теперь работают

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import org.jetbrains.compose.compose
 
+version = "1.6"
+
 plugins {
     kotlin("jvm") version "1.5.31"
     id("org.jetbrains.compose") version "1.0.0"
@@ -18,6 +20,11 @@ dependencies {
     implementation(
         "io.github.microutils:kotlin-logging-jvm:2.0.11")
     runtimeOnly("org.slf4j:slf4j-simple:1.7.29")
+
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+    implementation ("org.jetbrains.kotlin:kotlin-reflect:1.5.31")
+    testImplementation ("org.jetbrains.kotlin:kotlin-test:1.5.31")
+    testImplementation ("org.jetbrains.kotlin:kotlin-test-junit:1.5.31")
 }
 
 compose.desktop {
