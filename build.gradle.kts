3745080a916c22469bee2b38869bb8a9ae4c1654
Maksim <erolaz@outlook.com>
2021-12-06 21:55:03 +0300

    Добавил поддержку compose!!!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,27 @@
+import org.jetbrains.compose.compose
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+    id("org.jetbrains.compose") version "1.0.0"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
 dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.2.0") //for JVM platform
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.1")
     implementation(
         "io.github.microutils:kotlin-logging-jvm:2.0.11")
     runtimeOnly("org.slf4j:slf4j-simple:1.7.29")
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+    }
 }
